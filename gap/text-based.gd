769b4419425814bf936a7abbf1b0d7a4af249f0d
Murray Greig <mjg31@st-andrews.ac.uk>
2025-03-10 14:37:08 +0000

    Declares the functions required for sets and matching any character

--- gap/text-based.gd
+++ gap/text-based.gd
@@ -1,5 +1,18 @@
-DeclareGlobalFunction( "reg_Example" );
+############################################################################
+##
+## text-based.gd
+##
+############################################################################
+##
+## This file declares functions for regular expression matching using an NFA
+## constructed by Thompson's construction algorithm
+##
+############################################################################
+
+
 DeclareGlobalFunction( "thompsons_nfa" );
+DeclareGlobalFunction( "set_nfa" );
+DeclareGlobalFunction( "any_nfa" );
 DeclareGlobalFunction( "literal_nfa" );
 DeclareGlobalFunction( "concatenate_nfa" );
 DeclareGlobalFunction( "question_nfa" );
@@ -14,5 +27,6 @@ DeclareGlobalFunction( "convert_to_postfix" );
 DeclareGlobalFunction( "is_operator" );
 DeclareGlobalFunction( "precedence" );
 DeclareGlobalFunction( "format_expression" );
-DeclareGlobalFunction( "display_Automaton");
+DeclareGlobalFunction( "display_Automaton" );
+DeclareGlobalFunction( "text_findall" );
 DeclareGlobalFunction( "text_Match" );
